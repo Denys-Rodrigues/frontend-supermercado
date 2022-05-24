a3b16d510814f1eb7f034745e09c61cc6ae2e109
DW Rodrigues <denys.w.r@hotmail.com>
2022-05-24 16:02:03 -0300

    Projeto Frontend Supermercado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FrmClienteComponent } from './frm-cliente/frm-cliente.component';
+import { LstClientesComponent } from './lst-clientes/lst-clientes.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FrmClienteComponent,
+    LstClientesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
